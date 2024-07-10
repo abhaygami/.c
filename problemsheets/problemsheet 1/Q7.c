670ea7e2e06af8ba44d064f20e131a7be88b9767
Abhay Gami <163134062+abhaygami@users.noreply.github.com>
2024-07-10 20:20:11 +0530

    check the code

---
+++ problemsheets/problemsheet 1/Q7.c
@@ -0,0 +1,39 @@
+
+#include<stdio.h>
+int isPrime(int);
+int main()
+{
+	printf("7. Write a function to check whether a given number is prime number or not");
+	int n,r;
+	printf("\nEnter number : ");
+	scanf("%d",&n);
+	r=isPrime(n);
+	if(r==1)
+	{
+		printf("%d is prime number ",n);
+	}
+	else
+	{
+		printf("%d is not prime number ",n);
+	}
+	return 0;
+}
+int isPrime(int num)
+{
+	int i,cnt=0;
+	for(i=1;i<=num;i++)
+	{
+		if(num%i==0)
+		{
+			cnt++;
+		}
+	}
+	if(cnt==2)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
