59c53f7fa338166345fb889742e2b0af92778c01
Abhay Gami <163134062+abhaygami@users.noreply.github.com>
2024-07-11 21:12:04 +0530

    check the code

---
+++ problemsheets/problemsheet 1/Q9.c
@@ -0,0 +1,22 @@
+
+
+#include<stdio.h>
+int factorial(int);
+int main()
+{
+	printf("9. Write a function to display return factorial of integer n.");
+	int n;
+	printf("\nEnter number : ");
+	scanf("%d",&n);
+	printf("%d! = %d",n,factorial(n));
+	return 0;
+}
+int factorial(int num)
+{
+	int i,f=1;
+	for(i=num;i>=1;i--)
+	{
+		f=f*i;
+	}
+	return f;
+}
