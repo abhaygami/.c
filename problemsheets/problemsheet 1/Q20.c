dd3ed4fdc36b618f63c879cd2df04bc1567fd4d4
Abhay Gami <163134062+abhaygami@users.noreply.github.com>
2024-07-14 14:36:31 +0530

    check the code

---
+++ problemsheets/problemsheet 1/Q20.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+int sumdigit(long long int);
+int main()
+{
+	printf("20. Write a function to calculate sum of digits in the number N.\n");
+	long long int n;
+	int count;
+	printf("Enter number N : ");
+	scanf("%Ld",&n);
+	count=sumdigit(n);
+	printf("Sum of digits in %Ld = %d",n,count);
+	return 0;
+}
+int sumdigit(long long int num)
+{
+	int cnt,sum=0;
+	while(num!=0)
+	{
+		cnt=num%10;
+		sum=sum+cnt;
+		num=num/10;
+	}
+	return sum;
+}
