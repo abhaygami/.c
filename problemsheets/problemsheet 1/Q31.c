06d1a45177a1c8713c2717da9fc41f9009db4015
Abhay Gami <163134062+abhaygami@users.noreply.github.com>
2024-07-16 20:51:50 +0530

    check the code

---
+++ problemsheets/problemsheet 1/Q31.c
@@ -0,0 +1,31 @@
+
+
+#include<stdio.h>
+double power(int,int);
+int main()
+{
+    printf("30 Write user defined function calculate power of number n using recursion.\n");
+    int a,n;
+    double cal;
+    printf("Enter the base number: ");
+    scanf("%d",&a);
+    printf("Enter power : ");
+    scanf("%d",&n);
+    cal=power(a,n);
+    printf("%d raise to %d is %lf",a,n,cal);
+}
+double power(int x,int y)
+{
+    if(y==0)
+    {
+        return 1;
+    }
+    else if(y<0)
+    {
+        return 1/(x*power(x,-y-1));
+    }
+    else
+    {
+        return  x*power(x,y-1);
+    }
+}
