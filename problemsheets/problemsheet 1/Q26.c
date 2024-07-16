621b6f99723af8e0c500122ffd441a816a2a9c20
Abhay Gami <163134062+abhaygami@users.noreply.github.com>
2024-07-16 20:44:49 +0530

    check the code

---
+++ problemsheets/problemsheet 1/Q26.c
@@ -0,0 +1,46 @@
+
+
+#include<stdio.h>
+void p(int);
+int main()
+{
+	int n;
+	printf("Enter n = ");
+	scanf("%d",&n);
+	p(n);
+	return 0;
+}
+void p(int n)
+{
+	int i,j;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<2*n;j++)
+		{
+			if(j<=n)
+			{
+				if(i+j>=n+1)
+				{
+					printf("%d ",j-n+i);
+				}
+				else
+				{
+					printf("  ");
+				}
+			}
+			else
+			{
+				if(j-i<n)
+				{
+					printf("%d ",j-n+i);
+				}
+				else
+				{
+					printf("  ");
+				}
+			}
+		}
+		printf("\n");
+	}
+	return;
+}
